f244164e2f86d17256faf72c5370f68d55acf524
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-19 16:59:11 +0000

    Tue, Mar 19, 2024, 9:59 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,5 +1,15 @@
 rules = document.querySelector('.rules')
+rbtn = document.querySelector('.rules-btn')
+cbtn = document.querySelector('.close-btn')
 
-rules.addEventListener('click', () => {
+
+rbtn.addEventListener('click', () => {
     rules.classList.add('show')
 })
+
+cbtn.addEventListener('click', () => {
+    rules.classList.remove('show')
+})
+
+
+
