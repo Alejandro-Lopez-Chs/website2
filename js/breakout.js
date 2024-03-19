c5f77ebff58b5a635228d72e1956102e45beb121
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-19 17:25:51 +0000

    Tue, Mar 19, 2024, 10:25 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -12,13 +12,12 @@ ball = {
     speed: 4,
     dx: 4,
     dy: -4,
-
 }
 
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath()
-    ctx.arc(ball.x, ball.y, 50, 0, Math.PI * 2, true)
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
     ctx.stroke()
 }
 
