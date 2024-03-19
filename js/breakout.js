34fdb1543903bb8bf4c6eb8f84cf2fb485520d8d
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-19 17:10:45 +0000

    Tue, Mar 19, 2024, 10:10 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -2,7 +2,7 @@ rules = document.querySelector('.rules')
 rbtn = document.querySelector('.rules-btn')
 cbtn = document.getElementById('close-btn')
 
-
+// RUles open and close event handlers
 rbtn.addEventListener('click', () => {
     rules.classList.add('show')
 })
