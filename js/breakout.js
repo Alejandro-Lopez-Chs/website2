0c76e7f48ad72976fedf4dbb1effe115e775b40b
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-03-19 17:18:16 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,26 @@
 rules = document.querySelector('.rules')
 rbtn = document.querySelector('.rules-btn')
 cbtn = document.getElementById('close-btn')
+canvas = document.getElemtntById('canvas')
+ctx = canvas.getContext('2d')
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+
+}
+
+// Draw ball on canvas
+function drawBall() {
+    
+}
+
+
 
 // RUles open and close event handlers
 rbtn.addEventListener('click', () => {
