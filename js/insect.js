6de2c681ca4f0341478b3b4800a39c767026feb8
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-04-10 16:07:26 +0000

    Wed, Apr 10, 2024, 9:07 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -21,5 +21,6 @@ choose_insect_btns.forEach(btn => {
         const src = img.getAttribute('src')
         screens[1].classList.add('up')
         selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
     })
 })
