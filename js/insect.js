1304ff192eaf9c54e32371a06a4de54f98edd30e
Alejandro Lopez <142828677+Alejandro-Lopez-Chs@users.noreply.github.com>
2024-04-10 15:52:33 +0000

    Wed, Apr 10, 2024, 8:52 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,2 +1,7 @@
 const screens = document.querySelectorAll('.screen')
-
+const choose_insect_btns = document.querySelectorALL('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementByID('score')
+const message = document.getElementById('message')
